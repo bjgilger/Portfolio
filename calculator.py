19cdd4788c0e26adb4b5ea79cf92d3d6f9f254be
John Gilger <john.gilger@gmail.com>
2025-05-18 23:47:23 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,25 @@
+print("Earned amount: ")
+print("Bubblegum: $202")
+print("Toffee: $118")
+print("Ice cream: $2250")
+print("Milk chocolate: $1680")
+print("Doughnut: $1075")
+print("Pancake: $80")
+print()
+print("Income: $5405.0")
+
+# Get expenses
+staff_expenses = int(input("Enter staff expenses: "))
+other_expenses = int(input("Enter other expenses: "))
+
+print(f"Staff expenses: ${staff_expenses}")
+print(f"Other expenses: ${other_expenses}")
+
+print(f"Net income: ${5405 - staff_expenses - other_expenses}")
+
+
+
+
+
+
+
